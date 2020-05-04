07dbcefb4f4a724efe75c92a0978f8e42440b40e
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-05-04 16:55:12 +0200

    Allow usage as subproject

--- cmake/DefineProjectVersion.cmake
+++ cmake/DefineProjectVersion.cmake
@@ -12,7 +12,7 @@
 
 if (EASYHTTPCPP_VERBOSE_MESSAGES)
     message(STATUS)
-    message(STATUS "################# ${CMAKE_PROJECT_NAME} version details #################")
+    message(STATUS "################# ${PROJECT_NAME} version details #################")
 endif ()
 
 file(STRINGS "${PROJECT_SOURCE_DIR}/libversion" SHARED_LIBRARY_VERSION)
@@ -22,7 +22,7 @@ file(STRINGS "${PROJECT_SOURCE_DIR}/VERSION" PACKAGE_VERSION)
 add_definitions(-DPACKAGE_VERSION=${PACKAGE_VERSION})
 
 if (EASYHTTPCPP_VERBOSE_MESSAGES)
-    message(STATUS "${CMAKE_PROJECT_NAME} package version: ${PACKAGE_VERSION}")
+    message(STATUS "${PROJECT_NAME} package version: ${PACKAGE_VERSION}")
 endif ()
 
 string(REGEX REPLACE "([0-9]+)\\.[0-9]+\\.[0-9]+.*" "\\1" CPACK_PACKAGE_VERSION_MAJOR ${PACKAGE_VERSION})
@@ -41,16 +41,16 @@ add_definitions(-DPACKAGE_VERSION_PATCH=${CPACK_PACKAGE_VERSION_PATCH})
 add_definitions(-DPACKAGE_VERSION_EXT=${CPACK_PACKAGE_VERSION_EXT})
 
 if (EASYHTTPCPP_VERBOSE_MESSAGES)
-    message(STATUS "${CMAKE_PROJECT_NAME} package version major: ${CPACK_PACKAGE_VERSION_MAJOR}")
-    message(STATUS "${CMAKE_PROJECT_NAME} package version minor: ${CPACK_PACKAGE_VERSION_MINOR}")
-    message(STATUS "${CMAKE_PROJECT_NAME} package version patch: ${CPACK_PACKAGE_VERSION_PATCH}")
-    message(STATUS "${CMAKE_PROJECT_NAME} package version extensions: ${CPACK_PACKAGE_VERSION_EXT}")
+    message(STATUS "${PROJECT_NAME} package version major: ${CPACK_PACKAGE_VERSION_MAJOR}")
+    message(STATUS "${PROJECT_NAME} package version minor: ${CPACK_PACKAGE_VERSION_MINOR}")
+    message(STATUS "${PROJECT_NAME} package version patch: ${CPACK_PACKAGE_VERSION_PATCH}")
+    message(STATUS "${PROJECT_NAME} package version extensions: ${CPACK_PACKAGE_VERSION_EXT}")
 endif ()
 
 set(PROJECT_VERSION ${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH})
 
 if (EASYHTTPCPP_VERBOSE_MESSAGES)
-    message(STATUS "${CMAKE_PROJECT_NAME} project version: ${PROJECT_VERSION}")
-    message(STATUS "################# ${CMAKE_PROJECT_NAME} version details #################")
+    message(STATUS "${PROJECT_NAME} project version: ${PROJECT_VERSION}")
+    message(STATUS "################# ${PROJECT_NAME} version details #################")
     message(STATUS)
 endif ()
